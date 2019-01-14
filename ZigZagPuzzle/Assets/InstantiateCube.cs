58edbb9b3eecefb995ae8b00e346ecda80aa9cb6
MTLChrisLEE <sanghyukchrislee@gmail.com>
2019-01-14 15:59:47 -0500

    When the space bar is pressed, a cube is generated

---
+++ ZigZagPuzzle/Assets/InstantiateCube.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateCube : MonoBehaviour
+{
+    public Transform prefab;
+    int counter = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+               
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(prefab, new Vector3(-15 + counter * 3.0F, 0, 0), Quaternion.identity);
+            counter++;
+        }
+    }
+}
