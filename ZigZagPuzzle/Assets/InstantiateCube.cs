788491a3552bf6ea0d0566c3018ebae988a93f76
MTLChrisLEE <sanghyukchrislee@gmail.com>
2019-01-14 17:10:08 -0500

    The cubes are generated automatically

--- ZigZagPuzzle/Assets/InstantiateCube.cs
+++ ZigZagPuzzle/Assets/InstantiateCube.cs
@@ -10,16 +10,13 @@ public class InstantiateCube : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-               
+        InvokeRepeating("CreateNewCube", 3.0F, 1.0F);     
     }
 
-    // Update is called once per frame
-    void Update()
+   
+    public void CreateNewCube()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Instantiate(prefab, new Vector3(-15 + counter * 3.0F, 0, 0), Quaternion.identity);
-            counter++;
-        }
+        Instantiate(prefab, new Vector3(-15 + counter * 3.0F, 0, 0), Quaternion.identity);
+        counter++;
     }
 }
