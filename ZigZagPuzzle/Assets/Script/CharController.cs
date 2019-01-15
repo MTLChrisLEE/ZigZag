2a11ab51b9f7c57ce5dbebe32b94d869727739ec
MTLChrisLEE <sanghyukchrislee@gmail.com>
2019-01-14 23:04:46 -0500

    Basic setup for ZIGZAG game

---
+++ ZigZagPuzzle/Assets/Script/CharController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharController : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+    private bool walkingRight = true;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ChangePosition();
+        }
+        
+    }
+
+   private void FixedUpdate()
+    {
+        rigidbody.position = transform.position + transform.forward * 2 * Time.deltaTime;
+    }
+
+    private void ChangePosition()
+    {
+        this.walkingRight = !this.walkingRight;
+
+        if(this.walkingRight)
+        {
+            transform.rotation = Quaternion.Euler(0, 45, 0);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, -45, 0);
+        }
+
+    }
+}
